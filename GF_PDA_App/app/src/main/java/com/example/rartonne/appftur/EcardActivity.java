7ea45e227cc23f199c5fd54072a1e859e5346a5d
amachado <amachado@adents.com>
2015-06-10 14:28:16 +0200

    Commit header et footer

--- GF_PDA_App/app/src/main/java/com/example/rartonne/appftur/EcardActivity.java
+++ GF_PDA_App/app/src/main/java/com/example/rartonne/appftur/EcardActivity.java
@@ -1,35 +1,23 @@
 package com.example.rartonne.appftur;
 
-import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 
-public class EcardActivity extends Activity {
+public class EcardActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public void buttonOnClick (View v){
-        //Action si on clique sur le boutton
-        //Button button=(Button) v;
-
-        ((TextView)findViewById(R.id.hello)).setText("Coucou");
+        setContentView(R.layout.activity_ecard);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_ecard, menu);
         return true;
     }
 
